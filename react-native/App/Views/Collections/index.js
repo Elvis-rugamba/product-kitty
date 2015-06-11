0827691f18b7a25497ec10392649615d0b5ef589
Richard Kho <rkho@me.com>
2015-06-11 13:04:59 -0700

    Scaffolding Collections component

---
+++ react-native/App/Views/Collections/index.js
@@ -0,0 +1,12 @@
+var React = require('react-native');
+var styles = require('./styles.js');
+
+var CollectionsCell = require('./CollectionsCell');
+
+var {
+ View,
+ Text,
+ Image,
+ TouchableHightlight,
+ ActivityIndicatorIOS
+} = React;
